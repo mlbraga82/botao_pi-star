81e3e9bfee9a8953864dd917366cf9e3515796fd
Marcos Braga <39124251+mlbraga82@users.noreply.github.com>
2020-09-07 10:17:14 -0300

    Update botao_service.py

--- botao_service.py
+++ botao_service.py
@@ -9,12 +9,8 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 prev_input=0
 print ("Entrando no modo loop...")
 while True:
-#       print('.')
         input=GPIO.input(10)
-#       print ("input:",input)
         if ((not prev_input) and input):
-                print("button pressed")
                 os.system("python3 last_tg.py")
-#               os.system('ssh pi-star@py7slb.local ./deltg.sh')
         prev_input=input
         time.sleep(0.05)
